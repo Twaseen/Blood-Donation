d65d71f0a492d189147baff04a5562319ded45d1
Twaseen <twaseen47@gmail.com>
2024-01-08 21:52:26 +0600

    signup-control

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:final_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:final_app/splash_screen.dart';
 import '1stpage.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
---
+++ lib/signup_controller.dart
@@ -0,0 +1,18 @@
+import 'package:final_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:final_app/regpage.dart';
+
+class SignUpController extends GetxController {
+  static SignUpController get instance => Get.find();
+  final FullName = TextEditingController() ;
+  final BloodGroup = TextEditingController() ;
+  final ContactNumber = TextEditingController() ;
+  final Age = TextEditingController() ;
+  final Password = TextEditingController() ;
+
+  void registerUser(String email,String password){
+
+  }
+}
