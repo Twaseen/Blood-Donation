71b75496128590f8984809a8526f1df9e1824911
mimjk <afiasiddiquemim@gmail.com>
2024-01-20 17:53:01 +0600

    Dashboard

---
+++ lib/dashboard.dart
@@ -0,0 +1,151 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class dashboard extends StatelessWidget {
+  const dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dashboard',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 30),
+                  title: Text(
+                    'Dashboard',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        ?.copyWith(color: Colors.white),
+                  ),
+                  trailing: CircleAvatar(
+                    radius: 30,
+                    // Use your preferred user icon here
+                    child: Icon(Icons.person, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100),
+                ),
+              ),
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 20, // Adjust as needed
+                mainAxisSpacing: 20, // Adjust as needed
+                children: [
+                  itemDashboard(
+                    'Search',
+                    Icons.search,
+                    Colors.deepOrange,
+                  ),
+                  itemDashboard(
+                    'Post',
+                    Icons.post_add,
+                    Colors.deepOrange,
+                  ),
+                  itemDashboard(
+                    'Donor',
+                    Icons.people,
+                    Colors.deepOrange,
+                  ),
+                  itemDashboard(
+                    'Foundation',
+                    Icons.account_balance,
+                    Colors.deepOrange,
+                  ),
+                  itemDashboard(
+                    'Bank',
+                    Icons.business,
+                    Colors.deepOrange,
+                  ),
+                  itemDashboard(
+                    'Help ',
+                    Icons.help,
+                    Colors.deepOrange,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget itemDashboard(String title, IconData iconData, Color background) =>
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 5),
+              color: Theme.of(context).primaryColor.withOpacity(.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+            )
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: background,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(iconData, color: Colors.white),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.subtitle1,
+            )
+          ],
+        ),
+      );
+}
