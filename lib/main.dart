92385e986f5c7a0578369c56a8ee4b43906809bd
Twaseen47 <140600557+Twaseen@users.noreply.github.com>
2024-01-21 05:01:43 +0600

    edit_profile

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.system,
-      home: SplashScreen(),
+      home: const Dashboard(),
     );
   }
 }
